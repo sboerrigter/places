fda8f9c82528855085468d22a8eaed586c664a17
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-04-02 18:13:13 +0200

    🔧 Webpack

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
 <title>Places</title>
 
-<link href="https://fonts.googleapis.com/css?family=Lato:300,700|Pacifico" rel="stylesheet">
-<link href="/css/app.css" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,700|Pacifico">
+<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,14 @@
 const { mix } = require('laravel-mix');
 
+mix.disableNotifications();
+
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css', {
+     outputStyle: 'compressed'
+   })
+   .sourceMaps()
+   .browserSync('places.localhost');
+
+if (mix.config.inProduction) {
+    mix.version();
+}
